6cba32901d12958180fdd70f558ddd29155d5124
Aldrin Lijo <aldrinlijo04@gmail.com>
2025-04-12 20:04:15 +0530

    lijo 1st commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        port: process.env.DB_PORT,
+        logging: false, // Disable logging for cleaner output
+    }
+);
+
+module.exports = sequelize;
